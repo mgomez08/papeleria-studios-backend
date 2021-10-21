0177d7160f0a05b938d87dfbe1004726dde519e5
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-20 21:05:18 -0500

    Create basic configuration for app

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const { API_VERSION } = require("./config");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Configure header HTTP
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+module.exports = app;
