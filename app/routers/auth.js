79be68bebb6b73de0c7a90536c403f312ee529f1
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-30 22:39:22 -0500

    Use validateJWT middleware

--- app/routers/auth.js
+++ app/routers/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const AuthController = require("../controllers/auth");
+const { validateJWT } = require("../middleware/validateToken");
 
 const api = express.Router();
 
 api.post("/signUp", AuthController.signUp);
 api.post("/signIn", AuthController.signIn);
+api.post("/refreshToken", validateJWT, AuthController.refreshToken);
 
 module.exports = api;
