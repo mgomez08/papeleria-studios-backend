30017cec73883ea69fdc0969ec586a0d8b687ff5
Marlon Gómez <marlonjulian15@gmail.com>
2021-11-01 13:47:35 -0500

    Create Proveedor model

---
+++ app/models/Proveedor.js
@@ -0,0 +1,42 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Proveedor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Proveedor.init(
+    {
+      nom_prov: {
+        type: DataTypes.STRING(80),
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "El nombre del proveedor es requerido",
+          },
+        },
+      },
+      contacto_prov: {
+        type: DataTypes.STRING(80),
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "El contacto del proveedor es requerido",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "proveedor",
+      tableName: "proveedores",
+    }
+  );
+  return Proveedor;
+};
