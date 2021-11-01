67115a36367422a75df1d318b5c39a73647a6637
Marlon Gómez <marlonjulian15@gmail.com>
2021-11-01 15:35:49 -0500

    Added all controllers in Provider router

--- app/routers/provider.js
+++ app/routers/provider.js
@@ -5,5 +5,8 @@ const { validateJWT } = require("../middleware/validateToken");
 const api = express.Router();
 
 api.post("/", validateJWT, ProviderController.createProvider);
+api.delete("/", validateJWT, ProviderController.deleteProvider);
+api.put("/", validateJWT, ProviderController.updateProvider);
+api.get("/", validateJWT, ProviderController.getProviders);
 
 module.exports = api;
