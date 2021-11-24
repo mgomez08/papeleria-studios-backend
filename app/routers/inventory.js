1cdecd2f5740514bbd7bf5ee888afaa5571d1795
Marlon Gómez <marlonjulian15@gmail.com>
2021-11-23 22:25:59 -0500

    Create inventory routes and use inventory controllers

---
+++ app/routers/inventory.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const InventoryController = require("../controllers/inventory");
+const { validateJWT } = require("../middleware/validateToken");
+
+const api = express.Router();
+
+api.post("/", validateJWT, InventoryController.createInventory);
+api.delete("/", validateJWT, InventoryController.deleteInventory);
+api.put("/", validateJWT, InventoryController.updateInventory);
+api.get("/", validateJWT, InventoryController.getInventory);
+
+module.exports = api;
