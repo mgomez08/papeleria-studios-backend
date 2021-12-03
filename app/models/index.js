8288a64d574356252f74f8db75a1d4d5fe639713
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-03 12:58:14 -0500

    Force true

--- app/models/index.js
+++ app/models/index.js
@@ -14,7 +14,7 @@ let sequelize = new Sequelize(
   config.password,
   config
 );
-// sequelize.sync({ alter: true });
+sequelize.sync({ force: true });
 //Associations
 fs.readdirSync(__dirname)
   .filter((file) => {
